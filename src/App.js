3cbcfd7796c34b707551fbb53ad93f5d58a87e26
unknown <thunderrk2001@gmail.com>
2023-01-10 19:59:50 +0530

    basename added

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
 import ParticleBackground from "./components/particleBg";
-import {BrowserRouter as Router,Route,Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './pages/home';
 import Info from './utils/userInfo';
-function App() {
-  return (
-    <Router>
-      <ParticleBackground></ParticleBackground>
-      <div>Navbar</div>
-      <Routes>
-        <Route  path='/' element={<Home />}></Route>
-        <Route  path='*' element={(<div>No Page exists...</div>)}>
-        </Route>
-      </Routes>
-    </Router>
-  )
-}
 
-export default App;
+function App() {
+    return ( <
+            Router basename = "/portfolio" >
+            <
+            ParticleBackground > < /ParticleBackground> <
+            div > Navbar < /div> <
+            Routes >
+            <
+            Route path = '/'
+            element = { < Home / > } > < /Route> <
+            Route path = '*'
+            element = {
+                ( < div > No Page exists... < /div>)}> <
+                    /Route> <
+                    /Routes> <
+                    /Router>
+                )
+            }
+
+            export default App;
